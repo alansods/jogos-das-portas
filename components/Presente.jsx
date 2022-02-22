7103e5712ced7a3fa86a8820c2889afc6010c15c
Alan Santos <alansods@gmail.com>
2022-02-22 17:39:53 -0300

    css e componente presente

---
+++ components/Presente.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Presente.module.css'
+
+export default function Presente() {
+  return (
+    <div className={styles.presente}>
+      <div className={styles.tampa}>Tampa</div>
+      <div className={styles.corpo}>Corpo</div>
+
+    </div>
+  )
+}
